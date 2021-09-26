f2c5b511398e4184e37fd8d38a02f9279ee41cdc
KazeroG <17680194+KazeroG@users.noreply.github.com>
2021-09-26 01:00:12 -0400

    first upoad

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+  trailingSlash: true,
+  eslint: { ignoreDuringBuilds: true },
+
+  env: {
+    CLOUDINARY_URL: '	https://api.cloudinary.com/v1_1/allowebs',
+    CLOUD_NAME: 'allowebs',
+    UPLOAD_PRESET: 'pkmt',
+    JWT_SECRET: 'adfjdsjfsdjfkladjfkj',
+  },
+}
